2a3382c688861febcc46e047d3f8ab8b74b6cad7
cwilson2121 <50244082+cwilson2121@users.noreply.github.com>
2019-05-10 12:52:52 -0400

    Add files via upload

--- Graph.java
+++ Graph.java
@@ -1,38 +1,41 @@
-/*Replace this comment with your name.*/
+//Paul Eppley
+
 import java.util.*;
+import java.io.*;
 
 public class Graph
 {
-    private boolean[][] adjacencyMatrix;
-    private int[][] adjacencyList;
-    private int[] inDegreeArray;
-    private int[] outDegreeArray;
-    private int vertexCount;
-    private int edgeCount;
+    //This class stores an undirected graph in both adjacency list and adjacency matrix formats.
+    //It also stores an array of degrees of vertices, a vertex-count and an edge-count.
+    //Note that if (u, v) is in the edge set, both (u, v) and (v, u) will appear in the adjacency
+    //list.  Also, both adjacencyMatrix[u][v] and adjacencyMatrix[v][u] will be true.
+    protected boolean[][] adjacencyMatrix;
+    protected int[][] adjacencyList;
+    protected int[] degreeArray;
+    protected int vertexCount;
+    protected int edgeCount;
 
-    //This constructor reads in a directed graph from System.in.
+    //This constructor reads in an undirected graph from the specified file.
     //Precondition:  The input is correctly formatted as follows.
-    //    The first line contains an integer n representing the number of vertices
-    //    in the graph.  Subsequent lines contain the adjacency list representation
-    //    of the graph, i.e., an integer u followed by 0 or more integers v such that
-    //    (u, v) is an edge in the graph.  Vertices are numbered 0 through n - 1.
-    public Graph()
+    //    The first line contains an integer n representing the number of
+    //    vertices in the graph.  Subsequent lines contain edges.
+    //    Vertices are numbered 0 through n - 1.
+    public Graph(File graphFile) throws FileNotFoundException
     {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(graphFile);
         
         //Read the number of vertices from the first line.
         vertexCount = input.nextInt();
         
-        //Skip the rest of the line.
+        //Skip the rest of the first line.  (Anything else is garbage.)
         input.nextLine();
         
         //Allocate space for the adjacency list and matrix.
         adjacencyMatrix = new boolean[vertexCount][vertexCount];
         adjacencyList = new int[vertexCount][];
         
-        //Allocate space for the degree lists.
-        inDegreeArray = new int[vertexCount];
-        outDegreeArray = new int[vertexCount];
+        //Allocate space for the degree array.
+        degreeArray = new int[vertexCount];
 
         //Read in the remaining lines.
         while(input.hasNextLine())
@@ -42,7 +45,7 @@ public class Graph
             //Get the first integer on the line.  This represents the u in each edge (u, v).
             int u = line.nextInt();
             
-            //Get the remaining integers on the line.  These represent each v in (u, v).
+            //Get the remaining integers on the line.  This represent each v in (u, v).
             while(line.hasNextInt())
             {
                 int v = line.nextInt();
@@ -52,29 +55,94 @@ public class Graph
                 //If the edge hasn't already been read in somewhere else...
                 if(!adjacencyMatrix[u][v])
                 {
-                    outDegreeArray[u]++;
-                    inDegreeArray[v]++;
-                    adjacencyMatrix[u][v] = true;
+                    degreeArray[u]++;
+                    degreeArray[v]++;
+                    adjacencyMatrix[u][v] = adjacencyMatrix[v][u] = true;
                     edgeCount++;
                 }
             }
         }    
 
-        //We now have everything except the adjacency list.  Create that now from the
-        //adjacency matrix.
+        //We now have everything except the adjacency list.  Create that now.
         for(int u = 0; u < vertexCount; u++)
         {
-            adjacencyList[u] = new int[outDegreeArray[u]];
+            adjacencyList[u] = new int[degreeArray[u]];
            
             int degreeCounter = 0;
-              
+            
+            //March through row u of the adjacency matrix, inserting v into u's 
+            //adjacency list, if (u, v) is in the edge set.
             for(int v = 0; v < vertexCount; v++)
                 if(adjacencyMatrix[u][v])
                     adjacencyList[u][degreeCounter++] = v;
         }
-     }
-    
-    //This method writes the graph to System.out in the format specified above.
+    }
+
+    //This constructor reads in an undirected graph from System.in.
+    //Precondition:  The input is correctly formatted as follows.
+    //    The first line contains an integer n representing the number of
+    //    vertices in the graph.  Subsequent lines contain edges.
+    //    Vertices are numbered 0 through n - 1.
+    public Graph()
+    {
+        Scanner input = new Scanner(System.in);
+        
+        //Read the number of vertices from the first line.
+        vertexCount = input.nextInt();
+        
+        //Skip the rest of the first line.  (Anything else is garbage.)
+        input.nextLine();
+        
+        //Allocate space for the adjacency list and matrix.
+        adjacencyMatrix = new boolean[vertexCount][vertexCount];
+        adjacencyList = new int[vertexCount][];
+        
+        //Allocate space for the degree array.
+        degreeArray = new int[vertexCount];
+
+        //Read in the remaining lines.
+        while(input.hasNextLine())
+        {
+            Scanner line = new Scanner(input.nextLine());
+            
+            //Get the first integer on the line.  This represents the u in each edge (u, v).
+            int u = line.nextInt();
+            
+            //Get the remaining integers on the line.  This represent each v in (u, v).
+            while(line.hasNextInt())
+            {
+                int v = line.nextInt();
+                
+                //We now have the edge (u, v).  Process accordingly.
+                
+                //If the edge hasn't already been read in somewhere else...
+                if(!adjacencyMatrix[u][v])
+                {
+                    degreeArray[u]++;
+                    degreeArray[v]++;
+                    adjacencyMatrix[u][v] = adjacencyMatrix[v][u] = true;
+                    edgeCount++;
+                }
+            }
+        }    
+
+        //We now have everything except the adjacency list.  Create that now.
+        for(int u = 0; u < vertexCount; u++)
+        {
+            adjacencyList[u] = new int[degreeArray[u]];
+           
+            int degreeCounter = 0;
+            
+            //March through row u of the adjacency matrix, inserting v into u's 
+            //adjacency list, if (u, v) is in the edge set.
+            for(int v = 0; v < vertexCount; v++)
+                if(adjacencyMatrix[u][v])
+                    adjacencyList[u][degreeCounter++] = v;
+        }
+
+    }
+
+    //This methods writes the graph to System.out in adjacency list format.
     public void output()
     {
         System.out.println(vertexCount);
@@ -82,72 +150,22 @@ public class Graph
         for(int i = 0; i < vertexCount; i++)
         {
             System.out.print("" + i + " ");
-            for(int degree = 0; degree < outDegreeArray[i]; degree++)
+            for(int degree = 0; degree < degreeArray[i]; degree++)
                 System.out.print("" + adjacencyList[i][degree] + " ");
                 
             System.out.println();
         }
     }
-    
-    public ArrayList<Integer> DFS()
-    {
-        //The following ArrayList will hold DFS numbers and be returned by this method.  Both of the following 
-        //ArrayLists have an extra cell at the end to hold the current counter, so it can be passed to the recursive
-        //helper function and can be modified correctly.
-        ArrayList<Integer> dfsNumbers = new ArrayList<Integer>(vertexCount+1);
-        ArrayList<Integer> finishingNumbers = new ArrayList<Integer>(vertexCount+1);
-        
-        //Initialize all DFS numbers and all finishing numbers to 0.
-        for(int i = 0; i < vertexCount; i++)
-        {
-            dfsNumbers.add(0);
-            finishingNumbers.add(0);
-        }
-        
-        //Initialize the DFS number and finishing number counters to 1.
-        dfsNumbers.add(1);
-        finishingNumbers.add(1);
-            
-        //Perform the DFS on each unvisited vertex.
-        /*Replace this comment with the appropriate code.*/
-    }
-    
-    //Recursive DFS helper method
-    private void dfsHelper(int currentVertex, ArrayList<Integer> dfsNumbers, 
-                                              ArrayList<Integer> finishingNumbers)
-    {
-        //Visit the current vertex by setting its DFS number.
-        /*Replace this comment with the appropriate code.*/
-        
-        //Increment the DFS-number counter.
-        /*Replace this comment with the appropriate code.*/
-        
-        //March through the adjacency list of the current vertex.
-        for(/*Replace this comment with the appropriate code.*/)
-        {
-            /*Replace this comment with the appropriate code.*/
-            
-            //Identify and output edge types.
-            System.out.print("(" + u + ", " + v + ") : ");
 
-            //If v hasn't been visited yet...
-            if(/*Replace this comment with the appropriate code.*/)
-                System.out.println("Tree Edge");
-            //If v has been visited, but is not finished yet...    
-            else if(/*Replace this comment with the appropriate code.*/)
-                System.out.println("Back Edge");
-            //If v is visited and finished, but u was visited before v...
-            else if(/*Replace this comment with the appropriate code.*/)
-                System.out.println("Forward Edge");
-            //If v is visited and finished, but u was visited after v...
-            else
-                System.out.println("Cross Edge");
-            
-            //If the adjacent vertex hasn't yet been visited, recursively do a DFS on it.
-            /*Replace this comment with the appropriate code.*/
-        }
-        
-        //The current vertex's adjacency list is now used up.  Set its finishing number and increment the counter.
-        /*Replace this comment with the appropriate code.*/
+    //This method determines whether two vertices are adjacent.
+    public boolean areAdjacent(int u, int v)
+    {
+        return u != v && adjacencyMatrix[u][v];
+    }
+
+    //This method returns the vertex count.
+    public int getVertexCount()
+    {
+        return vertexCount;
     }
 }
